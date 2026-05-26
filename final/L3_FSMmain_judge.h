0ebe942c7f29086f08e529d8c526cf06a0356c7b
sungchaewon <scw226@sookmyung.ac.kr>
2026-05-26 16:53:48 +0900

    fix:플레이어 노드 4개로 변경

--- final/L3_FSMmain_judge.h
+++ final/L3_FSMmain_judge.h
@@ -8,7 +8,7 @@
 #define L3_JUDGE_STATE_IDLE             0
 #define L3_JUDGE_STATE_RUNNING          1
 
-#define L3_JUDGE_MAX_PARTICIPANTS       1
+#define L3_JUDGE_MAX_PARTICIPANTS       4
 #define L3_JUDGE_NICKNAME               L3_JUDGE_NICKNAME_STR
 #define L3_JUDGE_FIRST_TURN_TIMEOUT_SEC 30
 
