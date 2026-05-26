9bdf751eae0d94e6bd4237297c7fb1df0d4ac8a6
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2026-05-26 18:56:47 +0900

    Fix : debug control on으로 변경

--- final/protocol_parameters.h
+++ final/protocol_parameters.h
@@ -1,5 +1,5 @@
-#define DBGMSG_L2                       0 //debug print control
-#define DBGMSG_L3                       0 //debug print control
+#define DBGMSG_L2                       1 //debug print control
+#define DBGMSG_L3                       1 //debug print control
 
 #define L3_MAXDATASIZE                  1024
 
