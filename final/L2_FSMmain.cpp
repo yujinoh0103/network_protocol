e8f811a6cbf8f52260b24afffe0aa25afa7d28cd
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2026-05-26 19:18:46 +0900

    Fix : 디버그메시지 삭제

--- final/L2_FSMmain.cpp
+++ final/L2_FSMmain.cpp
@@ -197,7 +197,6 @@ void L2_FSMrun(void)
                 //L3_LLI_dataInd(L2_msg_getWord(dataPtr), srcId, size-L2_MSG_OFFSET_DATA, L2_LLI_getSnr(), L2_LLI_getRssi());
 #ifndef DISABLE_ARQ                
                 if (brflag == 0 && seqNum != L2_msg_getSeq(dataPtr)) {
-                    debug("[L3][WARNING] Invalid PDU SN (%i) while (%i) is required! (Ignoring SN check for multi-node)\n", L2_msg_getSeq(dataPtr), seqNum);
                 }
 #endif
                 L2_aggregateData(dataPtr, srcId, size, brflag, flag_end);
@@ -371,7 +370,6 @@ void L2_FSMrun(void)
                 //L3_LLI_dataInd(L2_msg_getWord(dataPtr), srcId, size-L2_MSG_OFFSET_DATA, L2_LLI_getSnr(), L2_LLI_getRssi());
 #ifndef DISABLE_ARQ                
                 if (brflag == 0 && seqNum != L2_msg_getSeq(dataPtr)) {
-                    debug("[L3][WARNING] Invalid PDU SN (%i) while (%i) is required! (Ignoring SN check for multi-node)\n", L2_msg_getSeq(dataPtr), seqNum);
                 }
 #endif
                 L2_aggregateData(dataPtr, srcId, size, brflag, flag_end);
